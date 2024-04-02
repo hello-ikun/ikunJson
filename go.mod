bb32ff99d0511f871f048f8c12a1885e9c46783a
Ikun <63904151+hello-ikun@users.noreply.github.com>
2024-04-02 21:31:48 +0800

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,3 @@
+module ikunJson
+
+go 1.21.6
---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bytes"
+	"compress/gzip"
+	"fmt"
+	"ikunJson/inner"
+	"io"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	s := []string{"twitter", "code", "example", "sample"}
+	for _, v := range s[:1] {
+		fp := fixture(v)
+		scan := inner.NewScanner(fp)
+		p := inner.NewParse(scan)
+		js := p.Json()
+		fmt.Println(inner.Tokens[js.Type], js.Value, js.Value)
+		fmt.Println()
+	}
+}
+
+// fuxture returns a *bytes.Reader for the contents of path.
+func fixture(path string) *bytes.Reader {
+	f, err := os.Open(filepath.Join("testdata", path+".json.gz"))
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	gz, err := gzip.NewReader(f)
+	if err != nil {
+		panic(err)
+	}
+	buf, err := io.ReadAll(gz)
+	if err != nil {
+		panic(err)
+	}
+	return bytes.NewReader(buf)
+}
